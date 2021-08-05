129c0c1675ad153c79172996bf19f8a28c7c4f6e
Marco0816 <82662826+Marco0816@users.noreply.github.com>
2021-08-05 15:34:49 +0800

    Add files via upload

---
+++ C29/link.js
@@ -0,0 +1,17 @@
+class link{
+    constructor(bodyA,bodyB)
+    {
+        var last_link = bodyA.body.bodies.length-2;
+        this.link = Constraint.create({
+
+           bodyA: bodyA.body.bodies[last_link],
+           pointA:{x:0,y:0},
+           bodyB: bodyB,
+           pointB:{x:0,y:0},
+           length: -60,
+           stiffness:0.01
+
+        });
+        World.add(world,this.link)
+    }
+}
